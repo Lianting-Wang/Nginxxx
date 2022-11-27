c79bce547698c3318e32b083ac9d81d735fbb434
王仔很忙 <wanglianting@vip.qq.com>
2022-11-27 10:29:22 -0500

    feat: Initializes the entrance to the entire program.

---
+++ ng_main.c
@@ -0,0 +1,44 @@
+#include "ng_main.h"
+
+int main() {
+	int i;
+	pid_t pid;
+	struct niginxxx_instance* niginxxx = ng_init_instance();
+	// printf("Niginxxx instance initialized success!\n");
+	struct port_list* port_lists = niginxxx->port_lists;
+	int sock_fd[niginxxx->lists_num];
+	for (i=0; i<niginxxx->lists_num; ++i) {
+		pid = fork(); //Create a child process for each virtual host
+		if (!pid) {
+			printf("I am pid %d for port %d\n", getpid(), port_lists->port);
+			sock_fd[i] = listening_port(port_lists->port);
+			break;
+		}
+		port_lists = port_lists->next;
+	}
+	if (!pid) {
+		int client_fd;
+    pid_t host_pid;
+		while(niginxxx) {
+			client_fd = accept_client(sock_fd[i]);
+			signal(SIGCHLD, SIG_IGN);
+			host_pid = fork(); //Create a new child process for each connection
+			if (!host_pid) {
+				handle_request(client_fd, niginxxx->hosts, port_lists->host_lists);
+				// close_socket(client_fd);
+				exit(0);
+			}
+		}
+	} else {
+		int status = 0;
+		wait(&status); //wait the end of child process
+		if(WIFEXITED(status)){
+			printf("child process return %d\n",WEXITSTATUS(status));
+		}
+		printf("I am pid %d\n", getpid());
+	}
+	// close_socket(sock_fd);
+	ng_destroy_instance(niginxxx);
+	printf("Niginxxx instance destroied success!\n");
+	return 0;
+}
---
+++ ng_main.h
@@ -0,0 +1,32 @@
+#ifndef NG_MAIN_H
+#define NG_MAIN_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "ng_readfile.h"
+
+/* forward declare */
+struct port_list;
+struct host_list;
+struct host_instance;
+struct niginxxx_instance;
+
+/* -- ng_readfile.c -- */
+struct niginxxx_instance* ng_init_instance();
+void ng_destroy_instance(struct niginxxx_instance* niginxxx);
+
+/* -- ng_web_socket.c -- */
+#define BACKLOG 20    // Maximum number of requests allowed in the request queue
+
+int listening_port(int port);
+int accept_client(int sock_fd);
+int close_socket(int sock_fd);
+
+/* -- ng_http.c -- */
+#ifndef NG_HTTP_H
+void handle_request(int client_fd, struct host_instance* hosts, struct host_list* host_lists);
+#endif
+
+#endif
