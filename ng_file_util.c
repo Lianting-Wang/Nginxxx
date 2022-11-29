12b00b05786d0e3822bfff60051cce6cf5b38c45
Lemon <38927629+Lemonsity@users.noreply.github.com>
2022-11-29 00:28:19 -0500

    add file util code

---
+++ ng_file_util.c
@@ -0,0 +1,23 @@
+#include "ng_file_util.h"
+
+int append_content_type(char * buf, char * path) {
+    const char point = '.';
+	char* suffix_name;
+	suffix_name = strrchr(buf, point);
+	if (suffix_name == NULL) { // Consider this please
+        strcat(buf, "Content-Type: text/plain; charset=utf-8\r\n");
+    } else if (strcmp(suffix_name, ".html") == 0) {
+		strcat(buf, "Content-Type: text/html; charset=utf-8\r\n");
+	} else if (strcmp(suffix_name, ".htm") == 0) {
+		strcat(buf, "Content-Type: text/html; charset=utf-8\r\n");
+	} else if (strcmp(suffix_name, ".txt") == 0) {
+		strcat(buf, "Content-Type: text/plain; charset=utf-8\r\n");
+	} else if (strcmp(suffix_name, ".jpg") == 0) {
+		strcat(buf, "Content-Type: image/jpeg\r\n");
+	} else if (strcmp(suffix_name, ".png") == 0) {
+		strcat(buf, "Content-Type: image/png\r\n");
+	} else if (strcmp(suffix_name, ".gif") == 0) {
+		strcat(buf, "Content-Type: image/gif\r\n");
+	}
+    return 0;
+}
---
+++ ng_file_util.h
@@ -0,0 +1,13 @@
+#ifndef NG_FILE_UTIL
+#define NG_FILE_UTIL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * given a path, select the correct
+*/
+int append_content_type(char * buf, char * path);
+
+#endif
